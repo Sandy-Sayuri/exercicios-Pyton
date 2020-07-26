63af33350237a48019d0f614e875171071f96698
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-26 13:53:14 -0300

    desafio completo
    Desefio 001 completo

--- exercicios/des001.py
+++ exercicios/des001.py
@@ -1,2 +1,3 @@
+# Crie um script Pyton que leia o nome de uma pessoas e mostre uma mensagem de acordo  com o valor digitado.
 nome=input('Qual é o seu nome')
-print(nome)
+print("Olá "+nome+" Prazer em te conhecer!")
---
+++ exercicios/des002.py
@@ -0,0 +1,4 @@
+dia=input('Dia = ')
+Mes=input('Mes = ')
+Ano=input('Ano = ')
+print('Você naceu no dia '+dia+" de "+Mes+" de "+Ano+". Correto?")
