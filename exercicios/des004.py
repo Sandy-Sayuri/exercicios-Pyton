e027aa082d875969175f628467b59282b2f21996
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-27 07:49:01 -0300

    des 004

---
+++ exercicios/des004.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia 2 números e mostre a soma entre eles
+n1=int(input('digite um número: '))
+n2=int(input('digite outro número: '))
+s=n1+n2
+print('A soma dos numeros {} e {} é {}'.format(n1,n2,s))
