354130fa5622b0450aecc5560dce2109c4c9f64f
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-27 11:39:04 -0300

    exe 007

---
+++ exercicios/des007.py
@@ -0,0 +1,4 @@
+#Desenvolva um progama que leia as duas notas de um aluno ,calcule e mostre a sua média.
+n1=float(input('digite um nota:'))
+n2=float(input('diguite outra nota'))
+print('a media emtre {} e {} é {}'.format(n1,n2,n1+n2/2))
