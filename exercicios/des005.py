8b0953725e004816b7f727de6e1c085d40ce09ae
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-27 11:14:08 -0300

    des 006

---
+++ exercicios/des005.py
@@ -0,0 +1,5 @@
+#Faça um programa que leia um número intero e mostre na tela seu sucessor e seu antecessor
+n1=int(input('digite um número: '))
+an=n1-1
+suc=n1+1
+print('O antecessor do número {} é {} e o sucessor é {}'.format(n1,an,suc))
