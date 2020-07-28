3933a0cfbd381037b6435e75bdad4a1a8b0b2854
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-28 10:44:28 -0300

    des10

---
+++ exercicios/des010.py
@@ -0,0 +1,5 @@
+#Crie um programa que leia quando dinhero uma pessoa tem na carteira e mostre quantos dolares ela pode comprar.
+#Concidere
+#US$1,00=R$3,27
+d=float(input('quanto dinhero vc tem na carteira '))
+print('Você pode comprar {} dolares'.format(d/3,27))
