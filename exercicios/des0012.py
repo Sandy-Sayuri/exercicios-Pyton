ab3bbc758883217cbb822f47e211bb5142a29620
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-29 08:52:37 -0300

    des012

---
+++ exercicios/des0012.py
@@ -0,0 +1,3 @@
+#Faça um algoritimo que leia o preço de um produto e mostre seu preço, com 5% de descomto.
+p=int(input('digite o preço do produto: '))
+print('o novo preço com descomto {}'.format(p-p*(5/100)))
