7ab578d1bc63e0630358d8d944b8a9b668e29343
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-29 11:34:35 -0300

    des013

--- exercicios/des013.py
+++ exercicios/des013.py
@@ -1,2 +1,3 @@
 #Faça um algoritimo que leia o salário de um funcionario e mostre seu novo salario, com 15% de aumento.
 s=int(input('Qual é o valor do salario: '))
+print('O salario novo é {}'.format(s+(s*(15/100))))
