c1aef71abf49dcb0e5508010343534131c7d6f90
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-29 08:38:44 -0300

    des0011

--- exercicios/des011.py
+++ exercicios/des011.py
@@ -1,1 +1,4 @@
-#Faça um programa que leia a altura e largura
+#Faça um programa que leia a altura e largura de uma parede em metros calcule a quantidade de tinta necessária para pintá-lo , sabendo que cada litro de tinta, pinta uma área de 2m2.
+a=int(input('Digite a altura da parede: '))
+l=int(input('digite a largura da parede: '))
+print('você vai preciar de {} latas de tinta'.format((l*a)*2))
