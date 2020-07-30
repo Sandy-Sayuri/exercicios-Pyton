250542a235dcccd5fd758a665227463fa660c0fb
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-30 14:24:45 -0300

    des014

---
+++ exercicios/des014.py
@@ -0,0 +1,3 @@
+#Escreva um programa que converta uma temperatura digitada em C e converter para F
+c=float(input('Digite a temperatura em C: '))
+print('A temrperatura em fahrenheit é {}'.format(c*1.8+32))
