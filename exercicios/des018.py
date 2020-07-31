c2f9b9e031c4906e6b9bdffbeabdc8b057dff072
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-31 12:08:13 -0300

    des018

---
+++ exercicios/des018.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia um ângulo qualquer e mostre na tela o valor de seno,cosseno e tangente desse ângulo
+import math
+a=int(input('Digite um ângulo : '))
+print('O seno do ângulo {}\n Cosseno : {} \nTangente:{}'.format(sen(a),a.cos,tan(a)))
