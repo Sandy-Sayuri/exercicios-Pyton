e26721227b3e5aed55757f3aabdc8e68891256d6
Sandy-sayuri <sandysayuri200@gmail.com>
2020-07-31 09:10:03 -0300

    des017 completo
    des016 tem que resolver ainda

---
+++ exercicios/des016.py
@@ -0,0 +1,2 @@
+#Crie um programa que leia um numero pelo teclado e mostre na teia a sua porção intera
+n=float(input('Digite um número: '))
---
+++ exercicios/des17.py
@@ -0,0 +1,5 @@
+#Faça um proguama que leia o comprimento do cateto oposto e do cateto oposto e do cateto adjasente de um triângulo,calcule e mostre o comprimento da hipotenusa
+from math import sqrt
+co=float(input('Digite o comprimeto do cateto oposto :'))
+ca=float(input('Digite o comprimeto do cateto adjacente: '))
+print('A tangente tem comprimento  de : ', sqrt((co*co)+(ca*ca)))
