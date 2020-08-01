dd6a5580c680478134e1158b841bbddd742f0ecf
Sandy-sayuri <sandysayuri200@gmail.com>
2020-08-01 13:38:28 -0300

    des021

---
+++ exercicios/des021.py
@@ -0,0 +1,7 @@
+#Faça um programa em Python que obra e reproduza o áudio de um arquivo MP3
+import pygame
+pygame.init()
+pygame.misic.load(#uma musica
+)
+pygame.mixer.music.play()
+pygame.event.wait()
