c2abb0b620b227b22dc5c68b715ab6da517f6512
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2021-02-26 13:20:08 -0300

    resolvido problemas

--- exercicios/des021.py
+++ exercicios/des021.py
@@ -1,7 +1,6 @@
 #Faça um programa em Python que obra e reproduza o áudio de um arquivo MP3
-import pygame
+import pygame 
 pygame.init()
-pygame.misic.load(#uma musica
-)
+pygame.misic.load(#uma musica)
 pygame.mixer.music.play()
 pygame.event.wait()
